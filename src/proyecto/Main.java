388827139b6ac636eb76d97f5a0a86c60b000eb3
Unknown <ana.castilla1998@gmail.com>
2018-11-20 15:34:40 +0100

    Prueba de readChar
    Prueba  de ejemplo del método readChar en la clase Main

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		/*
+		 * En esta clase se realizarán las pruebas manuales de los métodos de la clase Keyboard con ejemplos
+		 * para comprobar que hacen su función
+		 */
+		
+		//Leer un carácter de tipo char
+		System.out.print("Introduzca un carácter: ");
+		System.out.println(Keyboard.readChar());
 	}
 
 }
